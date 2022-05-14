6f7508b17c33061d3d73c779babafe25bb77c6ff
Marcelo Isola <marceloisola@gmail.com>
2022-05-14 16:05:30 -0300

    Se agrega el mock task con las tareas con su interface

---
+++ src/app/mock-task.ts
@@ -0,0 +1,29 @@
+
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer la aplicacion de Task",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Leer sobre Bootstrap",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer sobre Base de datos",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+    }
+]
